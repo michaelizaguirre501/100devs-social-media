4ae0c471cba8b8bc232ee890580a5055e3aaeca1
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-27 20:20:37 -0400

    trash

---
+++ app.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const ejs = require('ejs')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const connectdb = require('./config/database')
+const connectDB = require('./config/database')
+
+const app = express()
+connectDB()
+
+app.listen(
+    process.env.PORT,
+    console.log(`Server running on port ${process.env.PORT}`)
+)
